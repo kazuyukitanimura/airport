0d631cf5eb27d14f4e4c934d518e5ed1762fea7c
kazuyukitanimura <kazuyukitanimura@gmail.com>
2012-11-16 22:59:23 -0800

    example/secret was broken, fixed

--- example/secret/connect.js
+++ example/secret/connect.js
@@ -1,12 +1,11 @@
-var seaport = require('seaport');
 var airport = require('../../');
-
-var ports = seaport.connect('localhost', 9090);
-var air = airport(ports);
+var air = airport('localhost', 9090);
 
 var up = air.connect('beep');
-up(function (remote) {
-    remote.fives(11, function (n) {
-        console.log('fives(11) : ' + n);
+setInterval(function () {
+    up(function (remote) {
+        remote.fives(11, function (n) {
+            console.log('fives(11) : ' + n);
+        });
     });
-});
+}, 1000);
